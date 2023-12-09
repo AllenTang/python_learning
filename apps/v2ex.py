ca8770630bbb70a17951b642f1f2073a1ce1e084
taijia <taijia@pipapai.com>
2023-12-09 12:40:38 +0800

    requests suning

--- apps/v2ex.py
+++ apps/v2ex.py
@@ -1,5 +1,6 @@
 import requests
 from loguru import logger
+import json
 
 
 def fetch_main_page():
@@ -7,7 +8,10 @@ def fetch_main_page():
         'http': 'http://localhost:7890',
         'https': 'http://localhost:7890',
     }
-    url = 'https://www.scrapingbee.com/'
+    url = 'https://click.suning.cn/sa/jsConfig.action?dm=list.suning.com'
     logger.info('starting')
     resp = requests.get(url, proxies=proxies)
     logger.info('resp: {}', resp.text)
+    obj = json.loads(resp.text)
+    logger.info('resp: {}', obj.keys())
+
