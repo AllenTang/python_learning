3a46d82a87c962d9c1b73829114f4b97f4678f10
taijia <taijia@pipapai.com>
2023-12-16 15:43:27 +0800

    qzzn

--- apps/v2ex.py
+++ apps/v2ex.py
@@ -8,10 +8,8 @@ def fetch_main_page():
         'http': 'http://localhost:7890',
         'https': 'http://localhost:7890',
     }
-    url = 'https://click.suning.cn/sa/jsConfig.action?dm=list.suning.com'
+    url = 'https://bbs.qzzn.com/forum-29-1.html'
     logger.info('starting')
     resp = requests.get(url, proxies=proxies)
     logger.info('resp: {}', resp.text)
-    obj = json.loads(resp.text)
-    logger.info('resp: {}', obj.keys())
 
